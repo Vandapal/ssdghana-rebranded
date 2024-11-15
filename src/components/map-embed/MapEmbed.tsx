114359497b63d362853d719482563611ddca5766
Emmanuel Opoku-Ameyaw <110876611+the-pro7@users.noreply.github.com>
2024-11-15 20:57:31 +0000

    Update MapEmbed.tsx

--- src/components/map-embed/MapEmbed.tsx
+++ src/components/map-embed/MapEmbed.tsx
@@ -2,10 +2,10 @@ export default function MapEmbed() {
   return (
     <iframe
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125950.081639924!2d-0.9396812241422454!3d9.426659626126392!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfd43c903162fdeb%3A0x83f46c1562717cde!2sTamale!5e0!3m2!1sen!2sgh!4v1730944897789!5m2!1sen!2sgh"
-      width="600"
-      height="450"
+      width={600}
+      height={450}
       className="border-0"
-      allowfullscreen="true"
+      allowfullscreen={true}
       loading="lazy"
       referrerpolicy="no-referrer-when-downgrade"
     ></iframe>
